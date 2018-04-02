d2530a7c4a139a40978d8a10f9b1d589c737da24
Anna <anna@hydra.local>
2018-04-02 05:04:19 -0400

    write code to read and visualize csv

---
+++ visualize.py
@@ -0,0 +1,29 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+toread = ["Fake_Binary_Data.csv"] * 50 # reading the same file 50 times over
+data = []
+
+while len(toread) > 0:
+  binaryqs = {}
+  with open(toread[0], 'rt') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+      binaryqs[row[0]] = "yes" if random.random() < 0.5 else "no" # random inputs for now
+      # binaryqs[row[1]] = row[0] # the actual code
+  data.append(binaryqs)
+  del toread[0]
+
+# example bar graph for chicken pox
+yes = len([d for d in data if d['have chicken pox'] == 'yes'])
+
+x = np.arange(2)
+y = [yes, len(data) - yes]
+
+fig, ax = plt.subplots()
+plt.bar(x, y)
+plt.xticks(x, ('have had chicken pox', 'haven\'t had chicken pox'))
+plt.ylabel('number of people')
+plt.show()
